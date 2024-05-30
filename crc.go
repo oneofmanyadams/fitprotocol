c0de5a6b9fe6fc464ca38b3d8bc3162023a996ac
oneofmanyadams <adam.plazewski@gmail.com>
2024-05-29 22:30:23 -0600

    Added constructor function for CRC.

--- crc.go
+++ crc.go
@@ -1,10 +1,8 @@
 package fitprotocol
 
-import (
-	"errors"
-)
+import "errors"
 
-var CRC_BUFFER_OVERFLOW_ERROR = errors.New("Attempted to read outside of buffer.")
+var CRC_BUFFER_BOUNDS_ERROR = errors.New("Attempted to read outside of buffer.")
 
 var CRC_TABLE = []uint16{
 	0x0000, 0xCC01, 0xD801, 0x1400,
@@ -18,7 +16,13 @@ type CRC struct {
 	bytes_seen int
 }
 
-func (s *CRC) Get() uint16 {
+func CalculateCRC(buffer []byte, start, end int) (CRC, error) {
+	var crc CRC
+	err := crc.AddBytes(buffer, start, end)
+	return crc, err
+}
+
+func (s *CRC) CRC() uint16 {
 	return s.crc
 }
 
@@ -38,30 +42,11 @@ func (s *CRC) updateCRC(b byte) {
 
 func (s *CRC) AddBytes(buff []byte, start, end int) error {
 	if start < 0 || end >= len(buff) {
-		return CRC_BUFFER_OVERFLOW_ERROR
+		return CRC_BUFFER_BOUNDS_ERROR
 	}
 	for i := start; i <= end; i++ {
 		s.updateCRC(buff[i])
 		s.bytes_seen++
 	}
 	return nil
-}
-
-// Standalone func
-// need to convert to the crc in header/EOF to binary.LittleEndian.Uint16()
-// to compare against output of this function.
-func UpdateCRC(crc uint16, b byte) uint16 {
-	var tmp uint16
-
-	// compute checksum of lower four bits of byte
-	tmp = CRC_TABLE[crc&0xF]
-	crc = (crc >> 4) & 0x0FFF
-	crc = crc ^ tmp ^ CRC_TABLE[b&0xF]
-
-	// compute checksum of upper four bits of byte
-	tmp = CRC_TABLE[crc&0xF]
-	crc = (crc >> 4) & 0x0FFF
-	crc = crc ^ tmp ^ CRC_TABLE[(b>>4)&0xF]
-
-	return crc
 }
