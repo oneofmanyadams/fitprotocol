6c282d4603a8484539b96982f6b084a61519b8ff
oneofmanyadams <adam.plazewski@gmail.com>
2024-06-02 22:57:53 -0600

    Started on reader header method.

--- reader.go
+++ reader.go
@@ -16,16 +16,22 @@ const (
 )
 
 type FitReader struct {
-	Reader *io.Reader
+	Reader io.Reader
 	Buffer *bufio.Reader
 }
 
-func NewFitReader(reader *io.Reader) (FitReader, error) {
-	return FitReader{Reader: reader, Buffer: bufio.NewReader(*reader)}, nil
+func NewFitReader(reader io.Reader) (FitReader, error) {
+	return FitReader{Reader: reader, Buffer: bufio.NewReader(reader)}, nil
 }
 
-func (s *FitReader) HeaderSize() int {
-	return 0
+func (s *FitReader) HeaderSize() (int, error) {
+	// Should we just always grab the first byte of the file?
+	// And basically ignore the buffer or reader current location?
+	first_byte, peek_error := s.Buffer.Peek(1)
+	if peek_error != nil {
+		return 0, peek_error
+	}
+	return int(first_byte[0]), nil
 }
 
 func (s *FitReader) DataSize() int {
