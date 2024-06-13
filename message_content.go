6b7a77c2367bd00ad2809a16fd1b62e6a1024560
oneofmanyadams <adam.plazewski@gmail.com>
2024-06-12 22:05:11 -0600

    Created fixed content byte size constants and function for determing total fixed content size.

---
+++ message_content.go
@@ -0,0 +1,18 @@
+package fitprotocol
+
+const (
+	MSG_RESERVED_SIZE        = 1
+	MSG_ARCHITECTURE_SIZE    = 1
+	MSG_GLOBAL_NUM_SIZE      = 2
+	MSG_FIELD_COUNT_SIZE     = 1
+	MSG_FIELD_DEF_SIZE       = 3
+	MSG_FIELD_COUNT_SIZE_DEV = 1
+	MSG_FIELD_DEF_SIZE_DEV   = 3
+)
+
+func MsgFixedContentSize() int {
+	return MSG_RESERVED_SIZE +
+		MSG_ARCHITECTURE_SIZE +
+		MSG_GLOBAL_NUM_SIZE +
+		MSG_FIELD_COUNT_SIZE
+}
