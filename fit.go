7cba2768fb27b5fd4342b11309b076a0122e9cca
oneofmanyadams <adam.plazewski@gmail.com>
2024-06-18 22:19:13 -0600

    Function to name base type based on provided byte.

--- fit.go
+++ fit.go
@@ -19,3 +19,43 @@ const (
 	BASE_TYPE_UINT64  = 0x8F
 	BASE_TYPE_UINT64Z = 0x90
 )
+
+func BaseTypeName(b byte) string {
+	switch b {
+	case BASE_TYPE_ENUM:
+		return "ENUM"
+	case BASE_TYPE_SINT8:
+		return "SINT8"
+	case BASE_TYPE_UINT8:
+		return "UINT8"
+	case BASE_TYPE_SINT16:
+		return "SINT16"
+	case BASE_TYPE_UINT16:
+		return "UINT16"
+	case BASE_TYPE_SINT32:
+		return "SINT32"
+	case BASE_TYPE_UINT32:
+		return "UINT32"
+	case BASE_TYPE_STRING:
+		return "STRING"
+	case BASE_TYPE_FLOAT32:
+		return "FLOAT32"
+	case BASE_TYPE_FLOAT64:
+		return "FLOAT64"
+	case BASE_TYPE_UINT8Z:
+		return "UINT8Z"
+	case BASE_TYPE_UINT16Z:
+		return "UINT16Z"
+	case BASE_TYPE_UINT32Z:
+		return "UINT32Z"
+	case BASE_TYPE_BYTE:
+		return "BYTE"
+	case BASE_TYPE_SINT64:
+		return "SINT64"
+	case BASE_TYPE_UINT64:
+		return "UINT64"
+	case BASE_TYPE_UINT64Z:
+		return "UINT64Z"
+	}
+	return "NONE"
+}
