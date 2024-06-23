457e516740575a895bb965d38a1d25a69aa30daf
oneofmanyadams <adam.plazewski@gmail.com>
2024-06-23 17:29:58 -0600

    Created separate type for Message Data types.

---
+++ message_data_type.go
@@ -0,0 +1,10 @@
+package fitprotocol
+
+type DataType struct {
+	BaseTypeNumber int
+	EndianAbility  bool
+	BaseTypeField  uint8
+	Name           string
+	InvalidValue   int
+	Size           uint8
+}
