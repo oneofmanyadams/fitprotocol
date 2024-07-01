77220cd0e931e8a8ac902b9f07d506957fac1cd3
oneofmanyadams <adam.plazewski@gmail.com>
2024-06-30 21:53:59 -0600

    Added the message numbers that are used in the fit file being used for testing.

--- profile.go
+++ profile.go
@@ -12,4 +12,19 @@ type MsgNum struct {
 
 var MESSAGE_NUMBERS = MsgNums{
 	{Name: "file_id", Number: 0},
+	{Name: "hr_zone", Number: 8},
+	{Name: "power_zone", Number: 9},
+	{Name: "sport", Number: 12},
+	{Name: "session", Number: 18},
+	{Name: "lap", Number: 19},
+	{Name: "record", Number: 20},
+	{Name: "event", Number: 21},
+	{Name: "device_info", Number: 23},
+	{Name: "workout", Number: 26},
+	{Name: "activity", Number: 34},
+	// mesg 65281
+	// mesg 65284
+	// mesg 65285
+	{Name: "field_description", Number: 206},
+	{Name: "developer_data_id", Number: 207},
 }
