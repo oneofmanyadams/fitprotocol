d2592cde4e29d345eafe6272f1f9531c5e7b0a8e
oneofmanyadams <adam.plazewski@gmail.com>
2024-07-07 20:57:54 -0600

    Cleaned up the utilitu function for displaying definition messages.

--- util_fucs.go
+++ util_fucs.go
@@ -8,15 +8,17 @@ import "fmt"
 
 // Third func for reading timestamp messages?
 
-func ReadRecords(fit_reader *FitReader) {
+func ReadDefMsg(fit_reader *FitReader) DefinitionMessage {
 	fmt.Println("-------------------------------------------------")
 	// Read Message Type
 	msg_header_bytes, _ := fit_reader.ReadByte()
+	fmt.Println("")
 	fmt.Println("--Message Header:")
 	fmt.Printf("%+v\n", ParseMessageHeader(msg_header_bytes))
 	// read fixed length part of def message
 	fixed_msg_bytes, _ := fit_reader.ReadBytes(MsgFixedContentSize())
 	def_msg := NewDefinitionMessage(fixed_msg_bytes)
+	fmt.Println("")
 	fmt.Println("--Definition message:")
 	fmt.Printf("%+v\n", def_msg)
 	// read variable length part of data message
@@ -32,4 +34,6 @@ func ReadRecords(fit_reader *FitReader) {
 		}
 		fmt.Printf("%+v\n", data_type)
 	}
+	fmt.Println("")
+	return def_msg
 }
